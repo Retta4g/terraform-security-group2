4c22b31c6d3a583a3890852e7516e35dbb91ff67
Retta4g <messiwhanna@gmail.com>
2024-09-02 17:56:14 -0500

    new tag

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,3 +9,8 @@ variable "tag" {}
 output "my_vpc_id" {
   value = aws_vpc.my_vpc.id
 }
+# modules/security/outputs.tf
+
+output "web_security_group_id" {
+  value = aws_security_group.default["web"].id
+}
